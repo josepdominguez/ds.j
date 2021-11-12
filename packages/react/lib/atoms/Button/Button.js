7307490381193e548e75f1d60710697a4dc51b11
Josep Dominguez <jdominguez@dminc.com>
2021-11-12 16:26:40 +0100

    removing lib

--- packages/react/lib/atoms/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Button = ({ label, ...otherProps }) => {
-    return React.createElement("button", { ...otherProps }, label);
-};
-
-export { Button as default };
-//# sourceMappingURL=Button.js.map
